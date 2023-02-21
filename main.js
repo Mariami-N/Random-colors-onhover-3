5e6447c9e5e9e1308cd2bad4da8f11c20388476a
Mariami-N <avetianimari@gmail.com>
2023-02-20 23:23:02 -0800

    random color onhover finish (3) 2

--- main.js
+++ main.js
@@ -14,17 +14,21 @@ for (let i = 0; i < 500; i++) {
   square.style.backgroundColor = "rgb(24, 14, 30)";
   square.style.margin = '3px';
 
-
+let x;
   let colors = () => {
-  let randomColor = Math.floor(Math.random()*16777215).toString(16);
-  square.style.backgroundColor = "#" + randomColor;
+  let randomColor = "#" + Math.floor(Math.random()*16777215).toString(16);
+  x=randomColor;
+  square.style.backgroundColor = x;
+// square.style.boxShadow = "2px 2px 5px rgba(0, 0, 0, 0.3)";
   }
 
   square.addEventListener("mouseover", function(){
   colors();
+   square.style.boxShadow = "0px 0px 30px 4px" + x;
   })
-
+ 
   square.addEventListener('mouseout', function() {
   square.style.backgroundColor = "rgb(24, 14, 30)";
   square.style.transition="all 1s"
+  square.style.boxShadow = "none";
   })}
